36a4a1c6fa8a76c2cb20bad510ed231ad76f7999
rscbugtrack <rscbugtrack@gmail.com>
2017-11-19 17:59:03 +0000

    home page added with views

--- trackingapp/views.py
+++ trackingapp/views.py
@@ -4,8 +4,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def rschome(request):
-    html = ' <div style="text-align:center;"> <p  style="color:red;"> Welcome to RSC group </p> <br> \
-            <h3> This site is under maintenance </h3> </div> '
+from trackingapp.models import Bugtrack
 
-    return HttpResponse(html)
+# def rschome(request):
+#     html = ' <div style="text-align:center;"> <p  style="color:red;"> Welcome to RSC group </p> <br> \
+#             <h3> This site is under maintenance </h3> </div> '
+
+#     return HttpResponse(html)
+
+def rschome(request):
+    allrequirements = Bugtrack.objects.all()
+    context = {'allreq':allrequirements}
+
+    return render(request, 'rscbody.html', context)
